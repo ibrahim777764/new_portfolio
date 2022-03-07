aaf4079871fcde2679bd1844999e91070597beeb
Albert <albert.mensahansah@gmail.com>
2022-03-07 15:25:44 +0000

    Modify side navigation bar for mobile view

--- index.js
+++ index.js
@@ -38,14 +38,3 @@ function toggleModalVisibility() {
   sideMenuModal.classList.toggle('is-visible');
   mainPage.style.display = isVisible ? 'none' : 'block';
 }
-
-sideMenuButton.addEventListener('click', () => {
-  toggleModalVisibility();
-});
-
-menuItems.forEach((menuItem) => {
-  menuItem.addEventListener('click', (ev) => {
-    toggleModalVisibility();
-    window.location.hash = ev.target.getAttribute('data-location');
-  });
-});
