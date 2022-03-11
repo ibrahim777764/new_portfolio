049c0fbb3eb328d6b257bb9c796007b200a90c46
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-03-11 14:16:55 +0500

    updated html and css file

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ window.onload = () => {
   const closeDetailBtn = document.querySelector('#close-detail');
   const detailModal = document.querySelector('#mobile-details-modal');
   const seeProjectButtons = document.querySelectorAll('.see-project');
+  const form = document.querySelector('.form-input');
+  const [fullName, firstName, lastName] = form.elements;
+
+  const mediaqueryList = window.matchMedia('(max-width: 768px)');
+
+  const screenTest = (e) => {
+    if (e.matches) {
+      fullName.required = true;
+      firstName.required = false;
+      lastName.required = false;
+    } else {
+      fullName.removeAttribute('required');
+      firstName.required = true;
+      lastName.required = true;
+    }
+  };
+
+  screenTest(mediaqueryList);
+
+  mediaqueryList.addListener(screenTest);
+
+  const { email } = form.elements;
+  const errMsgEmail = document.querySelector('small');
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.add('hidden');
+    headerNavMobile.classList.remove('hidden');
+  });
 
   menuBtn.addEventListener('click', () => {
     menuBtn.classList.add('hidden');
@@ -95,6 +122,15 @@ window.onload = () => {
     link_to_source: 'https://github.com/ibrahim777764/new_portfolio/',
   },
   ];
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const regex = /[A-Z]/;
+    if (!regex.test(email.value)) {
+      form.submit();
+    } else {
+      errMsgEmail.innerText = 'Please enter only lower case';
+    }
+  });
 
   const gridTemplate = document.getElementById('grid-template');
   const recentWorks = document.getElementById('Recent-id');
