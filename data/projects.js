c2c5631fa17917f6da31941c1cd8cc11abf2f547
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-12-16 00:44:04 +0500

    updated portfolio version 2.0

---
+++ data/projects.js
@@ -0,0 +1,56 @@
+const projects = [{
+        title: " Space Travelers Hub ",
+        description: "In this task, we will be working with the real live data from the SpaceX API. our task is to build a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.",
+        imageUrl: "./20img/rocket.gif",
+        technologies: ["React", "Redux", "JavaScript", "HTML", "CSS"],
+        liveVersion: "https://vermillion-llama-7a5f4e.netlify.app/",
+        source: "https://github.com/ibrahim777764/Space-Travelers-Hub",
+    },
+
+    {
+        title: "PAKWHEELS CAR SHOW 2022",
+        description: "** PakWheels has been hosting auto shows in Karachi since 2015. Every year, we arrange this exciting event and call out all car lovers. An auto show, also known as a motor show or car show, is a public exhibition of current automobile models, debuts, concept cars, or out-of-production classics. It is attended by automotive industry representatives, dealers, auto journalists and car enthusiasts. Most auto shows occur once or twice a year**",
+        imageUrl: "./20img/CAR.gif",
+        technologies: ["JavaScript", "Webpack", "CSS", "Html"],
+        liveVersion: "https://ibrahim777764.github.io/project-capstone/",
+        source: "https://github.com/ibrahim777764/project-capstone",
+    },
+
+    {
+        title: "To-Do-List",
+        description: "To-do list is used to lists the things that you need to do and allows you to mark them as complete. This is built using ES6 and Webpack!",
+        imageUrl: "./20img/TODO.gif",
+        technologies: ["React", "Redux", "Jest", "JavaScript", "CSS", "Html"],
+        liveVersion: "https://fondem-jr.github.io/To-Do-list-list-structure/dist/",
+        source: "https://github.com/ibrahim777764/To-Do-list-list-structure",
+    },
+
+    {
+        title: "IBFLIX",
+        description: "It is a single page application named ibnas , we are fetching the tvMazaAPI to get all the data and displaying it on screen, the data is in the form of movies. The users can like, comments on movies and also can put reservation for future",
+        imageUrl: "./20img/MOVIE.gif",
+        technologies: ["ES6", "JavaScript", "CSS", "Html"],
+        liveVersion: "https://lucent-biscotti-de0616.netlify.app/",
+        source: "https://github.com/ibrahim777764/java-capstone",
+    },
+
+    {
+        title: "BUDGET-APP",
+        description: "The best budgeting apps help you understand your income and spending, so you have maximum control over your money",
+        imageUrl: "./20img/BUD.gif",
+        technologies: ["Ruby", "RAILS", "PostgreSql", "TailwindCSS"],
+        liveVersion: "",
+        source: "git@github.com:ibrahim777764/Budget-app.git",
+    },
+
+    {
+        title: "Recipe app",
+        description: "The Recipe app keeps track of all your recipes, ingredients, and inventory. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, the app should allow you to make them public so anyone can access them.",
+        imageUrl: "./20img/r.gif",
+        technologies: ["React", "JavaScript", "CSS", "ES6 modules", "JSX"],
+        liveVersion: "https://spiffy-platypus-4ee667.netlify.app/",
+        source: "https://github.com/ibrahim777764/Recipe-app",
+    },
+];
+
+export default projects;
